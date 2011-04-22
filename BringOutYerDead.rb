7a2795767e8653304753c569d5bf8c95b2d32151
ryber <geoit@geolearning.com>
2011-04-22 08:38:57 -0500

    Works Under OS X

--- BringOutYerDead.rb
+++ BringOutYerDead.rb
@@ -1,17 +1,19 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby
 
 # $ git diff --word-diff=porcelain --unified=0  365dcda1d9d90249a12b72d5c38d9cefe4860013 e914071bd374798b853d27f65df985d499a980fb
-
+require 'rubygems'
 require 'grit'
 include Grit
 
 puts "Hello WOrld"
 
-repo = Repo.new("/Maestro5/maestro/")
+repo = Repo.new("~/sites/Ninject-Examples/")
 
 #repo.commits.each do |commit|
 #		puts commit.headers
 #	end
 	
-repo.commit_diff('35cf41e9d093b63dd84b6c6a9995748872c9cfc3').count
+repo.commit_diff('7080cf24b07c27ece142e91aee4cc7d247318cd0').each do |dif|
+	puts dif.deleted_file
+end
 
